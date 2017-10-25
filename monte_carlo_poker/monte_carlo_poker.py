09e14b5feb38d264ff62cd2b7d5c7a7db62c8595
Rory Hartong-Redden <rory@gmail.com>
2017-10-24 20:37:33 -0700

    Running prep for web app

--- monte_carlo_poker/monte_carlo_poker.py
+++ monte_carlo_poker/monte_carlo_poker.py
@@ -1,4 +1,5 @@
 from functools import reduce
+import numpy as np
 
 class Card:
     def __init__(self, value, faceup=True, suit=None):
@@ -10,39 +11,55 @@ class Card:
 class Deck:
     def __init__(self, nb_decks=-1):
         self.nb_decks = nb_decks
+        self.card_values = [2,3,4,5,6,7,8,9,10,10,10,10,10]
 
+    def draw_card(self):
+        value = np.random.choice(self.card_values, 1)[0]
+        return(Card(value=value))
 
 class PokerPlayer:
-    def __init__(self, hand=[], deck=Deck()):
+    def __init__(self, dealer, hand=[], stay=False):
+        self.dealer = dealer
         self.hand = hand
+        self.stay = stay
 
     def hit(self):
-        self.hand.append(self.deck.draw_card())
+        self.hand.append(self.dealer.draw_card())
 
     def stay(self):
-        pass
+        self.stay = True
 
     @property
-    def total(self):
+    def sum(self):
         total_count = 0
         for card in self.hand:
             total_count += card.value
 
         return total_count
+    
+    @property
+    def isbusted(self):
+        if self.sum > 21:
+            return True
+        else:
+            return False
 
 
-class PokerDealer(PokerPlayer):
-    def __init__(self, hand=[], deck=Deck()):
+class PokerDealer:
+    def __init__(self, deck, hand=[]):
         self.deck = deck
+        self.hand = hand
 
     def draw_card(self):
-        return Card(4)
+        return self.deck.draw_card()
 
+    def deal_in(self):
+        return self.deck.draw_card().draw_card()
 
-card = Deck().draw_card()
-print(card.value)
+dealer = PokerDealer(Deck())
 
-p = PokerPlayer()
+p = PokerPlayer(dealer=dealer)
+p.deal_in()
+print(p.sum)
 p.hit()
-p.hit()
-print(p.total)
+print(p.sum)
