55ab1f115aaf63c78a4038525cffd5f73faf369c
Rory Hartong-Redden <rory@gmail.com>
2018-05-16 21:56:07 -0700

    Start adding sql

---
+++ sql/diamonds.sql
@@ -0,0 +1,20 @@
+CREATE TABLE diamond (
+	carat          real,
+	cut            varchar(80),
+	color          varchar(80),
+	clarity        varchar(80),
+	depth          int,
+	"table"        int,
+	price          real,
+	x              real,
+	y              real,
+	z              real
+);
+
+-- CREATE TABLE weather (
+--     city            varchar(80),
+--     temp_lo         int,           -- low temperature
+--     temp_hi         int,           -- high temperature
+--     prcp            real,          -- precipitation
+--     date            date
+-- );
