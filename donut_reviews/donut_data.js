e02623a1f76194f0ee9fee273b17f30ff7a5e179
Rory Hartong-Redden <rory@gmail.com>
2018-06-25 21:15:34 -0700

    Donut Reviews going online

---
+++ donut_reviews/donut_data.js
@@ -0,0 +1,46 @@
+var donutShop = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-121.9525295, 37.2809946]
+                
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "Maple Leaf Donuts"
+            },
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-121.5845063, 37.0138299]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "Gilroy Donut House"
+            },
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-121.6583271, 37.1327833]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "Donuts & Things"
+            },
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-122.0953746, 37.4140805]
+            },
+            "type": "Feature",
+            "properties": {
+                "popupContent": "Donut Basket"
+            },
+        },
+    ]
+};
